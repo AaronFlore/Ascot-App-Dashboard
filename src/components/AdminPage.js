0a0b2209232e49be470c12ec6ded1aa977e6794d
CSaltx <cobytyler7@gmail.com>
2023-11-27 14:23:13 -0800

    added basic forgot password page, added username db so name in admin portal, fixed sign up to align with previous, increased modularity

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -2,16 +2,33 @@ import React from "react";
 import Dropdown from "react-dropdown";
 import { auth } from "../firebaseConfig";
 import { fetchDataAndDownload } from "../csvOutput.js";
+import Image from "../images/user_icon.png";
 
-window.fetchDataAndDownload = fetchDataAndDownload;
+const AdminPage = ({ user, setUser }) => {
+  const handleSignOut = async () => {
+    try {
+      await auth.signOut();
+      setUser(null);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
-const AdminPage = () => {
-  const options = ["Plant Tracking App"];
-  const user = auth;
-  const name = user.currentUser.displayName ? user : "Dr. Willette";
+  const name =
+    user && user.firstName
+      ? `${user.firstName} ${user.lastName}`
+      : "Unknown User!";
 
   return (
     <div className="bg-[#0E0E0E] h-screen w-screen relative grid grid-cols-8 gap-7">
+      <div className="sign__out absolute">
+        <button
+          className="border-2 bg-black border-white rounded-full p-2 my-2 mx-2"
+          onClick={handleSignOut}
+        >
+          <p className="text-white font-bold">Sign Out</p>
+        </button>
+      </div>
       <div className="grid grid-rows-6 col-span-6 mt-10 ml-10 gap-8">
         <div className="w-full bg-[#D9D9D9] row-span-3 h-full rounded-[25px]"></div>
         <div className="w-full h-full row-span-2 rounded-[25px] grid grid-cols-6 gap-10">
@@ -20,11 +37,11 @@ const AdminPage = () => {
         </div>
       </div>
       <div className="bg-[#FCFF64] col-span-2 my-10 mr-7 rounded-[25px] grid grid-rows-2">
-        <div className="admin__user__logo flex justify-center items-center">
+        <div className="admin__user__logo flex flex-col  my-10 items-center">
           <img
-            src=""
+            src={Image}
             alt="User Profile Picture"
-            className="user__logo__img hidden border-black border-2"
+            className="user__logo__img w-[50%]"
           />
           <div className="font-bold">Welcome {name}!</div>
         </div>
--- src/components/LoginPortal.js
+++ src/components/LoginPortal.js
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 import firebase from "firebase/compat/app"; // Importing from compat/app
 import "firebase/compat/auth"; // Importing authentication module
-import { auth } from "../firebaseConfig";
+import { auth, db } from "../firebaseConfig";
+import { doc, setDoc, getDoc } from "firebase/firestore"; // Import methods to interact with Firestore
 import {
   setPersistence,
   browserLocalPersistence,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
 
 const LoginPortal = ({ user, setUser }) => {
   const [email, setEmail] = useState("");
   const [password, setPass] = useState("");
   const [error, setErrorMsg] = useState("");
   const [signUp, setSignUp] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   const handleSignUp = async () => {
     try {
@@ -28,12 +33,19 @@ const LoginPortal = ({ user, setUser }) => {
         email,
         password
       );
-      const currentUser = userCredential.user;
-      setUser({ displayName: currentUser.displayName || "" });
+
+      // Set user details in Firestore
+      await setDoc(doc(db, "users", email), {
+        firstName: firstName,
+        lastName: lastName,
+        admin: false, // Set default admin status
+      });
 
       setSignUp(false);
       setEmail("");
       setPass("");
+      setFirstName(""); // Clear the firstName state
+      setLastName(""); // Clear the lastName state
     } catch (error) {
       setErrorMsg(error.message);
       setTimeout(() => setErrorMsg(""), 1000);
@@ -48,14 +60,6 @@ const LoginPortal = ({ user, setUser }) => {
   const isValidEmail = (email) => {
     const regex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
     return regex.test(email);
-  };
-
-  const handleSignOut = async () => {
-    try {
-      await firebase.auth().signOut();
-    } catch (error) {
-      console.error(error.message);
-    }
   };
 
   const mapAuthCodeToMessage = (authCode) => {
@@ -82,8 +86,14 @@ const LoginPortal = ({ user, setUser }) => {
         email,
         password
       );
-      const currentUser = userCredential.user;
-      setUser({ displayName: currentUser.displayName || "" });
+      // Fetch user details from Firestore
+      const userDoc = await getDoc(doc(db, "users", email));
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+        setUser(userData);
+      } else {
+        throw new Error("User details not found.");
+      }
 
       // If persistence is required, you can add it here
     } catch (error) {
@@ -114,87 +124,29 @@ const LoginPortal = ({ user, setUser }) => {
       <div className="h-full flex items-center justify-start bg-[#D9D9D9]">
         <div className="h-[70%] w-[80vh] flex flex-col bg-[#F5F6FA] border-2 border-[#F5F6FA] border-l-0 p-8 items-center justify-center">
           {!signUp ? (
-            <>
-              <div className="input-container relative">
-                <input
-                  className="border border-t-0 border-l-0 border-r-0 text-black border-b-[#0F2B08] bg-[transparent] mb-4 p-2 focus:outline-none"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <label>Email/User</label>
-              </div>
-              <div className="input-container relative">
-                <input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPass(e.target.value)}
-                  onSubmit={handleLogin}
-                  className="border border-t-0 border-l-0 border-r-0 text-black border-b-[#0F2B08] bg-[transparent] mb-4 p-2 focus:outline-none"
-                />
-                <label>Password</label>
-              </div>
-              <div className="grid grid-cols-1">
-                {" "}
-                {/* FIXME: change to include pill for checkbox (grid-cols-2 and uncomment the below) */}
-                {/* <div className="w-[30%] mb-[1vh] scale-100 flex items-center justify-center">
-                  <div className="item h-full flex items-center justify-center">
-                    <div class="toggle-pill-color">
-                      <input type="checkbox" id="pill3" name="check" />
-                      <label htmlFor="pill3"></label>
-                    </div>
-                  </div>
-                </div> */}
-                <button
-                  title="submit"
-                  onClick={handleLogin}
-                  className="bg-[#0F2B08] text-white py-2 px-4 rounded"
-                >
-                  Submit
-                </button>
-              </div>
-              <div
-                className="text-blue-500 cursor-pointer mt-2"
-                onClick={() => setSignUp(true)}
-              >
-                Sign Up
-              </div>
-              <p className="text-red-500 mt-2 h-[1vh]">{error}</p>
-            </>
+            <SignIn
+              email={email}
+              setEmail={setEmail}
+              password={password}
+              setPass={setPass}
+              handleLogin={handleLogin}
+              setSignUp={setSignUp}
+              error={error}
+            />
           ) : (
-            <>
-              <div className="input-container">
-                <input
-                  className="border border-t-0 border-l-0 border-r-0 text-black border-b-[#0F2B08] bg-[transparent] mb-4 p-2 focus:outline-none"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <label>Email/User</label>
-              </div>
-              <div className="input-container">
-                <input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPass(e.target.value)}
-                  onSubmit={handleLogin}
-                  className="border border-t-0 border-l-0 border-r-0 text-black border-b-[#0F2B08] bg-[transparent] mb-4 p-2 focus:outline-none"
-                />
-                <label>Password</label>
-              </div>
-              <button
-                title="submit"
-                onClick={handleSignUp}
-                className="bg-[#0F2B08] text-white py-2 px-4 rounded"
-              >
-                Confirm
-              </button>
-              <p
-                className="text-blue-500 cursor-pointer mt-2"
-                onClick={() => setSignUp(false)}
-              >
-                ← Back
-              </p>
-              <p className="text-red-500 mt-2 h-[1vh]">{error}</p>
-            </>
+            <SignUp
+              firstName={firstName}
+              setFirstName={setFirstName}
+              lastName={lastName}
+              setLastName={setLastName}
+              email={email}
+              setEmail={setEmail}
+              password={password}
+              setPass={setPass}
+              handleSignUp={handleSignUp}
+              setSignUp={setSignUp}
+              error={error}
+            />
           )}
         </div>
       </div>
